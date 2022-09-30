3c3d2d9ecb1ca18d77c794caed3ee3eb8c976a2c
Nursultan <nsooonbekov65@gmail.com>
2022-09-30 12:20:15 +0900

    revew

---
+++ README.md
@@ -0,0 +1,2 @@
+# Review-website
+This is a review website in which you can see every person and their introductions.
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Azret Kenzhaliev',
+    job: 'web developer',
+    image:
+      'https://media-exp1.licdn.com/dms/image/C5603AQFIafmEccGHeA/profile-displayphoto-shrink_200_200/0/1624470719858?e=2147483647&v=beta&t=ZPFh-f_qVJCMG-iOGV_m23BNL1PnRJ521jrl4HbtdZ4',
+    text:
+      "Hello! I'm Azret, CS student @ KAIST, Technical Assessment Developer @ CodeSignal, ex-SWE Intern @ Palantir and Yelp, avid reader, skier (10 great years), and one of the many people who want to make this world a better place. "
+  },
+  {
+    id: 2,
+    name: 'Baiaman Bazarbaev',
+    job: 'Back-end developer',
+    image:
+      'https://media-exp1.licdn.com/dms/image/C5603AQHLYccUxpWScw/profile-displayphoto-shrink_800_800/0/1621046472715?e=2147483647&v=beta&t=QCHS-FyUhze5T5AScSYHKFN4wMiCs7795v9CLzvO_vg',
+    text:
+      'My name is Baiaman and currently, I am a senior student at KAIST. I am a back-end developer. I have a great experince in internships. So, please if you have any questions you can always contact me',
+  },
+  {
+    id: 3,
+    name: 'Baktynur Azhybaev',
+    job: 'Biology lab Assistant',
+    image:
+      'https://kabar.kg/site/assets/files/108854/a431c390-0ee2-4fbd-a60f-ea6031b997c7.jpg',
+    text:
+      'Hello, everyone! I am Baktynur. Currently, I am freshmen student in KAIST. I am very good at Biology and I am Bronze medalist in International Biology Olympiad (IBO)',
+  },
+  {
+    id: 4,
+    name: 'Omurbek Dzhamalov',
+    job: 'Full-stack Developer',
+    image:
+      'https://st-1.akipress.org/st_runews/.storage/limon3/images/FEVRALIO2022/cbd0b5818ce780ab52ea849bb05317f7.jpeg',
+    text:
+      'Hello! I am Omurbek, and I am a full-stack developer at Yandex. Besides, I am a Bronze medalist at International Olympiad in Informatics (IOI)',
+  },
+];
+
+export default reviews;
